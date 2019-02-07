f822c11f251881332f78ed7af34c7b07df26819e
Joshtg1104 <joshtg1104@gmail.com>
2019-02-07 16:56:31 -0600

    sheesh

---
+++ jqueryCW.js
@@ -0,0 +1,9 @@
+$(".red").addClass("rouge");
+$("#myElement").on("click", function () {
+    alert("My Element was clicked");
+    $("#myElement").addClass("blue")
+});
+$("#myElement div:nth-child(1)").addClass("backgroundGreen white");
+$("#myElement div:nth-child(2)").addClass("backgroundOrange pink");
+
+
